06a57c7b97e6134214dd16009e40ddef10dec993
bitesizing <r.raimundo2001@gmail.com>
2024-02-16 15:14:21 +0000

    FT: improved jsx flow, added full comments

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 // Define synthesised messages with isYours property !!
-const messages = [
-  { messageContent: "Hello", isYours: true },
-  { messageContent: "Hi there!", isYours: false },
-  { messageContent: "How are you?", isYours: true },
+const messageBlocks = [
+  {
+    isYours: true,
+    messageContents: ["Hello"],
+  },
+  {
+    isYours: false,
+    messageContents: ["How's it going?", "Bozo..."],
+  },
+  {
+    isYours: true,
+    messageContents: ["I hate you>:("],
+  },
 ];
 
 //  Main parent component
@@ -23,31 +32,33 @@ export default App;
 // MessageScreen component holding all the messages
 function MessageScreen() {
   return(
+    // Map function to create multiple MessageBlock components and assign messageContents to them
     <div className='messageScreen'>
-      {messages.map((message) => (
-        <Message isYours={message.isYours} messageContent={message.messageContent} />
+      {messageBlocks.map((messageBlock) => (
+        <MessageBlock isYours={messageBlock.isYours} messageContents={messageBlock.messageContents} />
       ))}
     </div>
   )
 }
 
-function Message({ isYours, messageContent }: { isYours: boolean; messageContent: string }) {
-  const isYoursIndicator: string = isYours? "right": "left"
-
-  return(
-    <div className = {"message " + isYoursIndicator}>
-      {Bubble(messageContent)}
-    </div>
-  )
-}
-
-function MessageBlock(isYours: boolean, messageContents: string[]) {
-  return Message(isYours, "hi")
-}
-
-function Bubble(messageContent: string) {
+// MessageBlock component holding all messages in a block (same owner)
+// Need to phrase the 'props' (properties) within curly brackets like this... not sure why, it's a React thing
+function MessageBlock({ isYours, messageContents }: { isYours: boolean; messageContents: string[] }) {
   return (
-    <div className = "message-bubble">
+    // Second map function to map each message in the block
+    <div className='messageBlock'>
+      {messageContents.map((message) => (
+        <Message isYours={isYours} messageContent={message} />
+      ))}
+    </div>
+  );
+}
+
+// Message component holding each individual message
+function Message({ isYours, messageContent }: { isYours: boolean; messageContent: string }) {
+  const isYoursIndicator: string = isYours? "right": "left"  // convert isYours boolean to string
+  return(
+    <div className = {"message" + " " + isYoursIndicator}>
       {messageContent}
     </div>
   )
