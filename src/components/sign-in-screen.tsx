af7c7466d5e6aa2f90caa05259db67de3de7e03f
ZebSK <147740847+ZebSK@users.noreply.github.com>
2024-03-05 12:23:02 +0000

    FT: customised sign-in with google button appearance

--- src/components/sign-in-screen.tsx
+++ src/components/sign-in-screen.tsx
@@ -1,5 +1,6 @@
 // Internal Modules
 import { signInWithGoogle } from "../services/auth"
+import '../styles/sign-in-screen.css';
 
 /** 
  * @file This module contains the sign-in screen that appears if not logged in
@@ -18,8 +19,9 @@ import { signInWithGoogle } from "../services/auth"
 function SignInScreen(): JSX.Element {
   // The JSX Element
   return (
-    <div>
-      <button onClick={signInWithGoogle}>
+    <div className="signInScreen">
+      <button className="googleButton" onClick={signInWithGoogle}>
+        <img className="googleLogo" src="http://1000logos.net/wp-content/uploads/2016/11/New-Google-Logo.jpg" alt="Google Logo"/>
         Sign in with Google
       </button>
 
